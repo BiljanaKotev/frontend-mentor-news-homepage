19f4d3e2307a9d6eed290528e14b518309b4a7f6
Biljana Kotevska <99471338+BiljanaKotev@users.noreply.github.com>
2022-12-08 16:22:14 +0100

    updated toggle function

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 function dropDown() {
-  const closeMenu = document.querySelector(".close-menu");
-  const navLinks = document.querySelector("#nav-links");
-  const ulFlex = document.querySelector(".ul-flex");
-  mobileMenu.classList.toggle("active");
-  closeMenu.classList.toggle("active");
-  navLinks.classList.toggle("active");
-  ulFlex.classList.toggle("active");
+  const ulMenu = document.querySelector(".ul-menu");
+
+  ulMenu.classList.toggle("active");
 }
 
-const mobileMenu = document.querySelector(".hamburger-menu");
-mobileMenu.addEventListener("click", dropDown);
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+hamburgerMenu.addEventListener("click", dropDown);
