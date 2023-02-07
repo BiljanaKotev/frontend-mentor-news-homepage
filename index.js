2869d01a13e4e66a8b8f133c6885358548a7da4d
Biljana Kotevska <99471338+BiljanaKotev@users.noreply.github.com>
2023-02-07 13:18:09 +0100

    updated the eventlistener and function code

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-function dropDown() {
-  const ulMenu = document.querySelector(".ul-menu");
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const closeMenu = document.querySelector(".mobile-menu-close");
+const ulMenu = document.querySelector(".ul-menu");
 
-  ulMenu.classList.toggle("active");
+hamburgerMenu.addEventListener("click", dropDown);
+closeMenu.addEventListener("click", dropDown);
+
+function dropDown() {
+  closeMenu.classList.toggle("active");
+  hamburgerMenu.classList.toggle("active");
+  if (ulMenu.style.display === "block") {
+    ulMenu.style.display = "none";
+  } else {
+    ulMenu.style.display = "block";
+  }
 }
 
-const hamburgerMenu = document.querySelector(".hamburger-menu");
-hamburgerMenu.addEventListener("click", dropDown);
